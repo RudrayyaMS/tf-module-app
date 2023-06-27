524de52f04649340e3d0c366c56b64da2d95eff3
Rudra <Rudrayyaswamims@gmail.com>
2023-06-27 23:29:48 +0530

    updating tf-module-app

--- main.tf
+++ main.tf
@@ -24,3 +24,15 @@ resource "aws_launch_template" "main" {
 
 // user_data = filebase64("${path.module}/example.sh")
 }
+
+## launch template versions
+resource "aws_autoscaling_group" "main" {
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+  vpc_zone_identifier = var.subnets                # subnet
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
