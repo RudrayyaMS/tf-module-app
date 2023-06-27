6f9f469fcdd4f6c807a13b451ceca4bde1e575f8
Rudra <Rudrayyaswamims@gmail.com>
2023-06-27 23:32:05 +0530

    updating tf-module-app

--- main.tf
+++ main.tf
@@ -27,12 +27,13 @@ resource "aws_launch_template" "main" {
 
 ## launch template versions
 resource "aws_autoscaling_group" "main" {
-  desired_capacity   = var.desired_capacity
-  max_size           = var.max_size
-  min_size           = var.min_size
+  desired_capacity    = var.desired_capacity
+  max_size            = var.max_size
+  min_size            = var.min_size
   vpc_zone_identifier = var.subnets                # subnet
 
   launch_template {
     id      = aws_launch_template.main.id
     version = "$Latest"
   }
+}
