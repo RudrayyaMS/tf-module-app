71ff9ce631deeff0947d2c2884e9242431c21f13
Rudra <Rudrayyaswamims@gmail.com>
2023-06-29 20:05:09 +0530

    updating tf-module-app

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 # policy creation
-resource "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "main" {
   name        = "${var.component}-${var.env}"
   path        = "/"
   description = "${var.component}-${var.env}"
