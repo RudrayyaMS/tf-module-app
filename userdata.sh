abe4a91b700e369d2fa82c2c2b83e2ce039e98be
Rudra <Rudrayyaswamims@gmail.com>
2023-06-29 07:59:36 +0530

    updating tf-module-app

--- userdata.sh
+++ userdata.sh
@@ -1,2 +1,2 @@
 
-ansibpe-pull -i localhist, -U htttps://github.com/RudrayyaMS/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
+ansibpe-pull -i localhost, -U htttps://github.com/RudrayyaMS/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
